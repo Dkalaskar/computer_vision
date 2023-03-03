d48f3900e1af04e8651a70bb077d4de675ce3f9b
Dhananjay Kalaskar <kalaskard123@gmail.com>
2023-03-03 10:59:38 +0530

    pratice how to change pixel in image

--- opencv1.py
+++ opencv1.py
@@ -5,9 +5,14 @@ import random
 img = cv2.imread('images/dhoni1.jfif',-1)
 
 #Change pixel in image
-for i in range (100):
-    for j in range(img.shape[1]):
-        img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+#for i in range (100):
+    #for j in range(img.shape[1]):
+        #img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+        
+tag = img[100:350,500:600]
+
+img[100:150, 110:100]  = tag
+       
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()        
